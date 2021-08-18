b156beb7229240fcfa02fec3734eea493de81266
SEbbaDK <sebbadk@gmail.com>
2021-08-18 22:19:33 +0200

    move logo and a add setup for communication

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import time
-import struct
+from struct import pack, unpack
 import krpc
 import serial
 import PySimpleGUI as sg
@@ -14,6 +14,18 @@ conn = krpc.connect()
 print("Connection established")
 
 vessel = conn.space_center.active_vessel
+
+# Test of resources
+while True:
+    time.sleep(1)
+    print("Sending data")
+    konsole.write(pack('<B', 3))
+    #konsole.write(pack('<B', 1))
+    #konsole.write(pack('<B', 0))
+    name = vessel.resources.names[0][0:8].encode()
+    print(f"Name: {name}")
+    #konsole.write(name)
+
 rcs_status = conn.add_stream(getattr, vessel.control, 'rcs')
 while True:
     if konsole.in_waiting > 1:
@@ -26,6 +38,6 @@ while True:
     result = 0
     if rcs_status():
         result |= 1
-    konsole.write(struct.pack('>B', result))
+    konsole.write(pack('>B', result))
     time.sleep(0.1)
 
